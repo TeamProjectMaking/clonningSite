677c5688159171b208bbbd6358b315c2a140f949
GREEN <GREEN@G704-00>
2024-01-31 11:37:53 +0900

    포폴 start! step01

---
+++ src/main/java/com/ex/KimssamSbEx18SecurityWebApplication.java
@@ -0,0 +1,13 @@
+package com.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimssamSbEx18SecurityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimssamSbEx18SecurityWebApplication.class, args);
+	}
+
+}
